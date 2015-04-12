319cc695eb34da35f489d72b1d66e56752300b2d
Jash Choraria <jashchoraria@hotmail.com>
2015-04-12 14:53:18 -0500

    Code for Nth value cross tab, similar to those given in class.

---
+++ 01 SQL Crosstabs/N_VALUE_AMOUNT.R
@@ -0,0 +1,6 @@
+N_VALUE_AMOUNT <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= 
+"SELECT ZIPCODE, GENDER, Amount, nth_value(Amount, 2)
+OVER (PARTITION BY ZIPCODE, GENDER orber by Amount) 2nd_AMOUNT 
+FROM Fed_Con_Zipcode
+order by 2,3 desc"
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_ac52722', PASS='orcl_ac52722', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
